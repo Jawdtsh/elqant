0a889054ff6fc62950506fa71c70887db831dd88
jawdt <jawdtalshalabe@gmail.com>
2023-12-14 07:32:09 +0200

    create project

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,19 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
 
-    public function index(): \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+    public function index(): Application|Factory|View
     {
         $products = Product::all();
         return view('product.all_products',compact('products'));
     }
 
 
-    public function create(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    public function create(): View|Factory|Application
     {
 
         return view('product.create');
@@ -25,7 +29,7 @@ class ProductController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
@@ -37,33 +41,29 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param Product $product
      * @return string
      */
-    public function show(Request $request)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @param $id
+     * @return Application|Factory|View
      */
     public function edit($id)
     {
         $product = Product::find($id);
         return view('product.edite',compact('product'));
-//        return $product;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Product $product
+     * @return RedirectResponse
      */
     public function update(Request $request,  $id)
     {
@@ -75,8 +75,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\RedirectResponse
+     * @param $id
+     * @return RedirectResponse
      */
     public function destroy($id)
     {
@@ -88,12 +88,12 @@ class ProductController extends Controller
         Product::withTrashed()->where('id',$id)->forceDelete();
         return redirect()->back();
     }
-    public function showme(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    public function showme(): Factory|View|Application
     {
         $products = Product::onlyTrashed()->get();
         return view('product.only_trashed',compact('products'));
     }
-    public function restore($id): \Illuminate\Http\RedirectResponse
+    public function restore($id): RedirectResponse
     {
         Product::onlyTrashed()->where('id',$id)->restore();
         return redirect()->back();
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home');
 })->name('home');
-Route::resource('product',ProductController::class);
+Route::resource('product',ProductController::class)->except([
+     'show'
+]);
 Route::get('product/forcedelete/{id}',[ProductController::class , 'forcdelete'])->name('product.forcedelete');
-Route::get('product/showme/softdelete',[ProductController::class,'showme'])->name('product.showme');
+Route::get('product/showme',[ProductController::class,'showme'])->name('product.showme');
 Route::get('product/restore/{id}',[ProductController::class,'restore'])->name('product.restore');
